aa699df78e461aa718491f94c72d8ef26b793fb7
DAVID MARKOV <markov.david1@gmail.com>
2024-03-11 11:59:07 +0100

    Web scrap nekretnine 11.03.2024. update

--- web_scraping_nekretnine.py
+++ web_scraping_nekretnine.py
@@ -1,74 +1,46 @@
-
-### Web scrapping of https://www.nekretnine.rs  ###
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-from datetime import *
+from datetime import datetime
 
-areas = []
-sq_meters = []
-room_numbers = []
-prices = []
-heatings = []
-floors = []
-total_floors = []
+BASE_URL = "https://www.nekretnine.rs/stambeni-objekti/stanovi/izdavanje-prodaja/izdavanje/grad/beograd/cena/1_1500/lista/po-stranici/10/stranica/"
 
-# Loop through first 30 pages
-for i in range(1, 31):
-    if i == 1:
-        url = "https://www.nekretnine.rs/stambeni-objekti/stanovi/izdavanje-prodaja/izdavanje/grad/beograd/cena/1_1500/lista/po-stranici/10/"
-    else:
-        url = "https://www.nekretnine.rs/stambeni-objekti/stanovi/izdavanje-prodaja/izdavanje/grad/beograd/cena/1_1500/lista/po-stranici/10/stranica/" + str(i) + "/"
+def fetch_apartment_links(url):
     result = requests.get(url)
     doc = BeautifulSoup(result.text, 'html.parser')
+    links = [link.get('href') for link in doc.select('.offer-title.text-truncate.w-100 a')]
+    return links
 
-    # Find all apartment links to loop through
-    elements_with_class = doc.find_all(class_ = "offer-title text-truncate w-100")
-    hrefs = []
-    for element in elements_with_class:
-        link_element = element.find('a')
-        if link_element:
-            href_value = link_element.get('href')
-            print(href_value)
-            hrefs.append(href_value)
-            
-    # Gathering data for all aparments
-    for link in hrefs:
-        url_app = "https://www.nekretnine.rs" + link
-        res = requests.get(url_app)
-        print(url_app)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        area = soup.find(class_ = "stickyBox__Location").text.strip()
-        areas.append(area)
-        sq = soup.find('span', text = "Kvadratura:")
-        sq_meter = sq.find_next('br').next_sibling.text.strip()
-        sq_meters.append(sq_meter)
-        print(sq_meter)
-        room = soup.find('span', text = "Sobe:")
-        room_num = room.find_next('br').next_sibling.text.strip()
-        room_numbers.append(room_num)
-        price = soup.find(class_ = "stickyBox__price").text.split('EUR')[0].strip()
-        prices.append(price)
-        heat = soup.find('span', text = "Grejanje:")
-        heating = heat.find_next('br').next_sibling.text.strip()
-        heatings.append(heating)
-        floor_info = soup.find('span', text = "Sprat:")
-        floor_text = floor_info.find_next('br').next_sibling.text.strip()
-        floor_text = floor_text.split("/")
-        floor = floor_text[0]
-        total_floor = floor_text[1]
-        floors.append(floor)
-        total_floors.append(total_floor)
+def scrape_apartment_data(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    area = soup.find(class_="stickyBox__Location").get_text(strip=True)
+    sq_meter = soup.find(lambda tag: tag.name == 'span' and "Kvadratura:" in tag.text).find_next('br').next_sibling.get_text(strip=True)
+    room_num = soup.find(lambda tag: tag.name == 'span' and "Sobe:" in tag.text).find_next('br').next_sibling.get_text(strip=True)
+    price = soup.find(class_="stickyBox__price").get_text(strip=True).split('EUR')[0].strip()
+    heating = soup.find(lambda tag: tag.name == 'span' and "Grejanje:" in tag.text).find_next('br').next_sibling.get_text(strip=True)
+    floor_text = soup.find(lambda tag: tag.name == 'span' and "Sprat:" in tag.text).find_next('br').next_sibling.get_text(strip=True).split("/")
+    floor, total_floor = floor_text if len(floor_text) == 2 else (None, None)
+    return {'area': area, 'sq_meters': sq_meter, 'room_number': room_num, 'price': price, 'heating': heating, 'floor': floor, 'total_floors': total_floor}
 
-df = pd.DataFrame({
-    'area' : areas,
-    'sq_meters' : sq_meters,
-    'room_number' : room_numbers,
-    'price' : prices,
-    'heating' : heatings,
-    'floor' : floors,
-    'total_floors' : total_floors
-})
-today = datetime.now().strftime("%d.%m.%y.")
-df.to_excel(f'Excel_files\\Data nekretnine {today}.xlsx', index=False)
-print(df)
+def scrape_data():
+    areas, sq_meters, room_numbers, prices, heatings, floors, total_floors = [], [], [], [], [], [], []
+    for i in range(1, 31):
+        url = BASE_URL + str(i) if i != 1 else BASE_URL
+        apartment_links = fetch_apartment_links(url)
+        for link in apartment_links:
+            apartment_data = scrape_apartment_data("https://www.nekretnine.rs" + link)
+            areas.append(apartment_data['area'])
+            sq_meters.append(apartment_data['sq_meters'])
+            room_numbers.append(apartment_data['room_number'])
+            prices.append(apartment_data['price'])
+            heatings.append(apartment_data['heating'])
+            floors.append(apartment_data['floor'])
+            total_floors.append(apartment_data['total_floors'])
+    return pd.DataFrame({'area': areas, 'sq_meters': sq_meters, 'room_number': room_numbers, 'price': prices, 'heating': heatings, 'floor': floors, 'total_floors': total_floors})
+
+if __name__ == "__main__":
+    df = scrape_data()
+    today = datetime.now().strftime("%d.%m.%y.")
+    df.to_excel(f'Excel_files\\Data nekretnine {today}.xlsx', index=False)
+    print(df)
